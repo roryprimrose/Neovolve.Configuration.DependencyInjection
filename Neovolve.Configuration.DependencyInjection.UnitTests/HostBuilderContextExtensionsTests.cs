32ff11e10e45cdff6d32c1e067a4b98ac3db4ff8
Rory Primrose <rory@neovolve.com>
2023-08-30 20:22:31 +1000

    Added dummy unit test project

---
+++ Neovolve.Configuration.DependencyInjection.UnitTests/HostBuilderContextExtensionsTests.cs
@@ -0,0 +1,11 @@
+namespace Neovolve.Configuration.DependencyInjection.UnitTests
+{
+    public class HostBuilderContextExtensionsTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
