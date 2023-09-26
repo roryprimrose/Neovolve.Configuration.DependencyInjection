d6f76f8d6a32e3f419a44de482afaef197798dc6
Rory Primrose <rory@neovolve.com>
2023-09-27 00:00:03 +1000

    Added ConfigureWith unit tests

--- Neovolve.Configuration.DependencyInjection.UnitTests/HostBuilderContextExtensionsTests.cs
+++ Neovolve.Configuration.DependencyInjection.UnitTests/HostBuilderContextExtensionsTests.cs
@@ -1,6 +1,10 @@
 namespace Neovolve.Configuration.DependencyInjection.UnitTests;
 
 using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using ModelBuilder;
 
 public class HostBuilderContextExtensionsTests
 {
@@ -10,5 +14,64 @@ public class HostBuilderContextExtensionsTests
         Action action = () => HostBuilderContextExtensions.ConfigureWith<Config>(null!);
 
         action.Should().Throw<ArgumentNullException>();
+    }
+
+    [Theory]
+    [MemberData(nameof(ConfigTypesDataSet), true)]
+    [MemberData(nameof(ConfigTypesDataSet), false)]
+    public void ConfigureWithConfiguresInjectionTypes(Type expected, bool configureReload)
+    {
+        var builder = Host.CreateDefaultBuilder();
+
+        builder.ConfigureAppConfiguration((_, configuration) =>
+        {
+            configuration.AddInMemoryCollection(
+                new Dictionary<string, string?>
+                {
+                    ["RootValue"] = "This is the root value",
+                    ["First:FirstValue"] = "This is the first value",
+                    ["First:Second:SecondValue"] = "This is the second value",
+                    ["First:Second:Third:ThirdValue"] = "This is the third value"
+                });
+        });
+
+        builder.ConfigureWith<Config>(configureReload);
+
+        using var host = builder.Build();
+
+        var actual = host.Services.GetService(expected);
+
+        actual.Should().NotBeNull();
+        actual.Should().BeAssignableTo(expected);
+    }
+
+    public static IEnumerable<object[]> ConfigTypesDataSet(bool configureReload)
+    {
+        yield return new object[]{typeof(Config), configureReload};
+        yield return new object[]{typeof(IConfig), configureReload};
+        yield return new object[]{typeof(FirstConfig), configureReload};
+        yield return new object[]{typeof(IOptions<FirstConfig>), configureReload};
+        yield return new object[]{typeof(IOptionsSnapshot<FirstConfig>), configureReload};
+        yield return new object[]{typeof(IOptionsMonitor<FirstConfig>), configureReload};
+        yield return new object[]{typeof(IFirstConfig), configureReload};
+        yield return new object[]{typeof(IOptions<IFirstConfig>), configureReload};
+        yield return new object[]{typeof(IOptionsSnapshot<IFirstConfig>), configureReload};
+        yield return new object[]{typeof(IOptionsMonitor<IFirstConfig>), configureReload};
+        yield return new object[]{typeof(SecondConfig), configureReload};
+        yield return new object[]{typeof(IOptions<SecondConfig>), configureReload};
+        yield return new object[]{typeof(IOptionsSnapshot<SecondConfig>), configureReload};
+        yield return new object[]{typeof(IOptionsMonitor<SecondConfig>), configureReload};
+        yield return new object[]{typeof(ISecondConfig), configureReload};
+        yield return new object[]{typeof(IOptions<ISecondConfig>), configureReload};
+        yield return new object[]{typeof(IOptionsSnapshot<ISecondConfig>), configureReload};
+        yield return new object[]{typeof(IOptionsMonitor<ISecondConfig>), configureReload};
+        yield return new object[]{typeof(ThirdConfig), configureReload};
+        yield return new object[]{typeof(IOptions<ThirdConfig>), configureReload};
+        yield return new object[]{typeof(IOptionsSnapshot<ThirdConfig>), configureReload};
+        yield return new object[]{typeof(IOptionsMonitor<ThirdConfig>), configureReload};
+        yield return new object[]{typeof(IThirdConfig), configureReload};
+        yield return new object[]{typeof(IOptions<IThirdConfig>), configureReload};
+        yield return new object[]{typeof(IOptionsSnapshot<IThirdConfig>), configureReload};
+        yield return new object[]{typeof(IOptionsMonitor<IThirdConfig>), configureReload};
     }
 }
